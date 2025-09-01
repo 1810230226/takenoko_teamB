7d2cff92b20e2dbd4005b25039dfec7be8a8f93c
Ryosuke <ryo656100@icloud.com>
2025-09-01 18:46:57 +0900

    step1の画面を実装(色等はまだ)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Top from "./pages/Top";
+import Top2 from "./pages/Top2";
 
 function App() {
-
   return (
-    <>
-    Hello</>
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/top" element={<Top />} />
+        <Route path="/top2" element={<Top2 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/Top2.tsx
@@ -0,0 +1,10 @@
+
+function Top2() {
+
+  return (
+    <>
+    OOOOOOO</>
+  );
+}
+
+export default Top2;
